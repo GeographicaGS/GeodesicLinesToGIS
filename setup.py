ade3e152b090c98a54b7b86a294513ec953d655f
cayetanobv <cayetanobv@gmail.com>
2015-11-09 01:51:14 +0100

    version 0.4 in docs

--- setup.py
+++ setup.py
@@ -12,28 +12,28 @@ from setuptools import setup, find_packages
 
 # Get the long description from README file.
 # Before upload a new version run rstgenerator.sh
-# to update Readme reStructuredText file from 
+# to update Readme reStructuredText file from
 # original Readme markdown file.
 with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
     name='GeodesicLinesToGIS',
-    version='0.3.2',
-    
+    version='0.4.0',
+
     description='Computes geodesic lines from start point to end point and stores them in a GIS file (Shapefile and GeoJSON). The problem of geodesic lines crossing antimeridian is solved.',
     long_description=long_description,
-    
+
     author='Cayetano Benavent',
     author_email='cayetano.benavent@geographica.gs',
-    
+
     # The project's main homepage.
     url='http://github.com/GeographicaGS/GeodesicLinesToGIS',
-    
+
     # Licensed under the GPLv2 license:
     # https://www.gnu.org/licenses/gpl-2.0.txt
     license='GPLv2',
-    
+
     # According to: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -43,9 +43,9 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: GIS'
     ],
-    
+
     keywords='geodesic GIS antimeridian',
-    
+
     packages=find_packages(),
     include_package_data=False,
     install_requires=[
