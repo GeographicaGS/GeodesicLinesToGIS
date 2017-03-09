07599ae2ab075d8fc1097bc527949de8c998e893
cayetanobv <cayetano.benavent@geographica.gs>
2017-03-10 00:25:02 +0100

    updated setup and manifest files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 # Licensed under the GPLv2 license:
 # https://www.gnu.org/licenses/gpl-2.0.txt
-# Copyright (c) 2015, Cayetano Benavent <cayetanobv@gmail.com>
+# Copyright (c) 2015-2017, Cayetano Benavent <cayetanobv@gmail.com>
 
 from setuptools import setup, find_packages
 
@@ -19,7 +19,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='GeodesicLinesToGIS',
-    version='0.4.2',
+    version='0.4.3',
 
     description='Computes geodesic lines from start point to end point and stores them in a GIS file (Shapefile and GeoJSON). The problem of geodesic lines crossing antimeridian is solved.',
     long_description=long_description,
@@ -40,6 +40,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: GIS'
     ],
